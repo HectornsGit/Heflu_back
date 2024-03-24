1de489471a62c33961913fe20387be6af861f6f4
FlorPM <florpm1991@gmail.com>
2024-03-24 11:59:36 +0100

    chore: adds env db variables in config file

--- config/env.js
+++ config/env.js
@@ -1,5 +1,7 @@
 import "dotenv/config"
 
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env
+
 const { PORT } = process.env || 3000
 
-export { PORT }
+export { PORT, MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE }
