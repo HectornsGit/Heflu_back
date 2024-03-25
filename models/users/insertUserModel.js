b593bc4087a03caa884846b789736158f759c703
HectornsGit <hector.novoa.segade@gmail.com>
2024-03-25 11:35:42 +0100

    feat adds the insertUserModel

--- models/users/insertUserModel.js
+++ models/users/insertUserModel.js
@@ -1,1 +1,34 @@
-//TO DO: insertUserModel
+import getDb from "../../config/getDb.js"
+
+const insertUserModel = async (
+    name,
+    email,
+    bio,
+    avatar,
+    password,
+    registrationCode
+) => {
+    let connection
+    try {
+        connection = await getDb()
+
+        let [users] = await connection.query(
+            `SELECT id FROM users WHERE email = ?`,
+            [email]
+        )
+
+        if (users.length > 0) {
+            const error = new Error("Ese email ya está registrado.")
+            error.statusCode = "409"
+        }
+
+        await connection.query(
+            `INSERT INTO users (email, name, bio, avatar, password, registrationCode) VALUES(?, ?, ?, ?, ?, ?)`,
+            [email, name, bio, avatar, password, registrationCode]
+        )
+    } finally {
+        if (connection) connection.release()
+    }
+}
+
+export default insertUserModel
