f9964f0261b6024c3356956b65b029211b98e0c8
HectornsGit <hector.novoa.segade@gmail.com>
2024-03-25 11:36:32 +0100

    feat adds the new user controller to the server

--- controllers/users/newUserController.js
+++ controllers/users/newUserController.js
@@ -1,1 +1,21 @@
-// TO DO: New user controller.
+import insertUserModel from "../../models/users/insertUserModel.js"
+
+const newUserController = async (req, res, next) => {
+    try {
+        const { name, email, bio, avatar, password } = req.body
+        const registrationCode = "patata"
+
+        await insertUserModel(
+            name,
+            email,
+            bio,
+            avatar,
+            password,
+            registrationCode
+        )
+    } catch (error) {
+        next(error)
+    }
+    res.send({ status: "ok", message: "Usuario creado" })
+}
+export default newUserController
