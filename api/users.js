2e54105ddadc89628a1065e58ce610be6861e3b3
HectornsGit <hector.novoa.segade@gmail.com>
2024-03-25 09:35:41 +0100

    feat (api): adds user endpoints

--- api/users.js
+++ api/users.js
@@ -1,1 +1,21 @@
-// TO DO: User endpoints
+import express from "express"
+
+const router = express.Router()
+
+router.post("/users", (req, res, next) => {
+    return "200 OK"
+})
+
+router.post("/users/validate/:regCode", (req, res, next) => {
+    return "200 OK"
+})
+
+router.post("/users/login", (req, res, next) => {
+    return "200 OK"
+})
+
+router.get("/users/:id", (req, res, next) => {
+    return "200 OK"
+})
+
+export default router
