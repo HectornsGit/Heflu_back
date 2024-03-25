27752f11c3c2994e09900c021976293cd7c24b4b
FlorPM <florpm1991@gmail.com>
2024-03-25 11:27:38 +0100

    chore: adds newProperty validation schema

---
+++ schemas/newPropertySchema.js
@@ -0,0 +1,21 @@
+import joi from "joi"
+import joiErrorMessages from "../joiErrorMessages.js"
+import imgSchema from "../imgSchema.js"
+
+const newPropertySchema = joi.object({
+    name: joi.string().required().messages(joiErrorMessages),
+    description: joi.string().max(200).required().messages(joiErrorMessages),
+    type: joi
+        .string()
+        .valid("Chalet", "Apartamento", "Casa rural", "Otros")
+        .messages(joiErrorMessages),
+    location: joi.string().max(50).required().messages(joiErrorMessages),
+    country: joi.string().max(50).required().messages(joiErrorMessages),
+    price: joi.number().required().messages(joiErrorMessages),
+    image: imgSchema.required().messages(joiErrorMessages),
+    area: joi.number().required().messages(joiErrorMessages),
+    bedrooms: joi.number().required().messages(joiErrorMessages),
+    bathrooms: joi.number().required().messages(joiErrorMessages),
+})
+
+export default newPropertySchema
