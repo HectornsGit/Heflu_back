5691b9d8bb81e0768259c44179e413b89c11f862
HectornsGit <hector.novoa.segade@gmail.com>
2024-03-25 10:09:15 +0100

    feat adds the endpoints to the server

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ import morgan from "morgan"
 
 import { PORT } from "#config/env"
 
+import userRoutes from "#api/userRoutes"
+
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(morgan("common"))
+
+app.use("/users", userRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server listening on: http://localhost:${PORT}`)
