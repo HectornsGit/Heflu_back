e1c4767e7f22523c0709572f8140a2e707ee2975
HectornsGit <hector.novoa.segade@gmail.com>
2024-04-01 10:22:26 +0200

    Feat: adds the profile controller.

--- api/userRoutes.js
+++ api/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express"
-
+import isUser from "../middlewares/isUser.js"
 import newUserController from "../controllers/users/newUserController.js"
 import activateUserController from "../controllers/users/activateUserController.js"
 import loginUserController from "../controllers/users/loginUserController.js"
+import getUserController from "../controllers/users/getUserController.js"
 
 const router = express.Router()
 
@@ -12,8 +13,6 @@ router.get("/validate/:registration_code", activateUserController)
 
 router.post("/login", loginUserController)
 
-router.get("/:id", (req, res, next) => {
-    res.send({ message: "200 OK" })
-})
+router.get("/:id", isUser, getUserController)
 
 export default router
