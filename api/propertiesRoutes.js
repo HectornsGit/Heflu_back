894614d1a38b24354d664e2066bffbc4561d1754
FlorPM <florpm1991@gmail.com>
2024-04-03 16:38:36 +0200

    feat: adds new route for get all properties

--- api/propertiesRoutes.js
+++ api/propertiesRoutes.js
@@ -1,8 +1,13 @@
 import express from "express"
+// Controllers
 import newPropertyController from "../controllers/properties/newPropertyController.js"
+import getAllPropertiesController from "../controllers/properties/getAllPropertiesController.js"
+
+// Middlewares
 import isUser from "../middlewares/isUser.js"
 
 const router = express.Router()
 
 router.post("/", isUser, newPropertyController)
+router.get("/", getAllPropertiesController)
 export default router
