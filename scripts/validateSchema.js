5d8d9d148af03466c5976623768f09595e3a244c
HectornsGit <hector.novoa.segade@gmail.com>
2024-04-03 11:19:03 +0200

    fix: Fixes the error management

--- scripts/validateSchema.js
+++ scripts/validateSchema.js
@@ -1,11 +1,13 @@
 // Validación de datos con JOI
+
+import generateError from "./generateError.js"
+
 // Pasar como parámetros el esquema y los datos a validar
 const validateSchema = async (schema, data) => {
     try {
         await schema.validateAsync(data)
     } catch (err) {
-        err.httpStatus = 400
-        throw err
+        throw generateError(400, err.message)
     }
 }
 
