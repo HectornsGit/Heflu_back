efcfdebcef369155789f79912fec7c3b1a24848a
HectornsGit <hector.novoa.segade@gmail.com>
2024-04-03 19:48:42 +0200

    feat (models): adds the insert bookings model

---
+++ models/bookings/insertBookingModel.js
@@ -0,0 +1,21 @@
+import getDb from "../../config/getDb.js"
+
+const insertPropertyModel = async (newBookingData) => {
+    const { starting_date, ending_date, tenant_id, property_id } =
+        newBookingData
+
+    let connection
+    try {
+        connection = await getDb()
+        const [newBooking] = await connection.query(
+            ` INSERT INTO bookings (starting_date, ending_date, is_confirmed, tenant_id, property_id)
+            VALUES(?,?,?,?,?)`,
+            [starting_date, ending_date, 0, tenant_id, property_id]
+        )
+        return newBooking.insertId
+    } finally {
+        if (connection) connection.release()
+    }
+}
+
+export default insertPropertyModel
