8804f1c5bcf5b7ff1ed3f42c62836a89cea0d9a0
FlorPM <florpm1991@gmail.com>
2024-04-04 18:27:45 +0200

    feat: adds route to view a property details

--- api/propertiesRoutes.js
+++ api/propertiesRoutes.js
@@ -2,6 +2,7 @@ import express from "express"
 // Controllers
 import newPropertyController from "../controllers/properties/newPropertyController.js"
 import getAllPropertiesController from "../controllers/properties/getAllPropertiesController.js"
+import getPropertyController from "../controllers/properties/getPropertyController.js"
 
 // Middlewares
 import isUser from "../middlewares/isUser.js"
@@ -10,4 +11,5 @@ const router = express.Router()
 
 router.post("/", isUser, newPropertyController)
 router.get("/", getAllPropertiesController)
+router.get("/:id", getPropertyController)
 export default router
