71defe8b066b67954bd30a92f2fd8bb1501e5961
HectornsGit <hector.novoa.segade@gmail.com>
2024-04-05 12:38:31 +0200

    feat(bookings): adds the booking deleting controller and route

--- api/bookingsRoutes.js
+++ api/bookingsRoutes.js
@@ -2,10 +2,11 @@ import express from "express"
 import isUser from "../middlewares/isUser.js"
 import newBookingController from "../controllers/bookings/newBookingController.js"
 import confirmBookingController from "../controllers/bookings/confirmBookingController.js"
+import deleteBookingController from "../controllers/bookings/deleteBookingController.js"
 
 const router = express.Router()
 
 router.post("/", isUser, newBookingController)
 router.put("/:id", isUser, confirmBookingController)
-router.delete("/:id")
+router.delete("/:id", isUser, deleteBookingController)
 export default router
