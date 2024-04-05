461c9e405b367b3ce6409bee3d14d6fc50e5c377
HectornsGit <hector.novoa.segade@gmail.com>
2024-04-05 12:10:21 +0200

    feat(mailing): Adds a script to send mail notifications about new confirmed bookings.

--- scripts/sendBookingConfirmationEmail.js
+++ scripts/sendBookingConfirmationEmail.js
@@ -1,18 +1,17 @@
 import { Resend } from "resend"
-import { PORT, RESEND_API_KEY } from "../config/env.js"
+import { RESEND_API_KEY } from "../config/env.js"
 
-const sendBookingNotificationEmail = (email, property) => {
+const sendBookingConfirmationEmail = (email, property) => {
     const resend = new Resend(RESEND_API_KEY)
     resend.emails.send({
         from: "heflu@resend.dev",
         to: [email],
-        subject: "Nueva reserva",
-        html: `<h1>Buenas, has recibido una nueva reserva</h1>
-        <h2><em><strong>${property.name} - ${property.location}</strong></em></h2>
-        <p><strong>Comprueba en el siguiente enlace tus reservas pendientes</strong></p>
-        <h1><span style="text-decoration: underline;"><a href="http://localhost:${PORT}/bookings/">RESERVAS</a></span></h1>
+        subject: "¡Reserva confirmada!",
+        html: `<h1>¡Buenas, tu reserva ha sido aceptada!</h1>
+        <h2><em><strong>Disfruta de tu estancia en</strong></em></h2>
+        <p><strong>${property.name} - ${property.location}</strong></p>
         <h2>HEFLÚ</h2>`,
     })
 }
 
-export default sendBookingNotificationEmail
+export default sendBookingConfirmationEmail
