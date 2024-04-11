3f6774469b37f680788abbcfa7371f2b99e8bd13
FlorPM <florpm1991@gmail.com>
2024-04-11 20:31:24 +0200

    feat: adds filters properties models

---
+++ models/properties/selectFilterPropertiesModel.js
@@ -0,0 +1,30 @@
+import getDb from "../../config/getDb.js"
+
+const selectFilterPropertiesModel = async (params, id) => {
+    let connection
+
+    const ids = id.ids.join(",").split(",").map(Number)
+
+    const country = params.country === undefined ? "%" : `%${params.country}%`
+    const maxPrice =
+        params.maxPrice === undefined ? "9999999999" : params.maxPrice
+    const minRooms = params.minRooms === undefined ? "0" : params.minRooms
+
+    try {
+        connection = await getDb()
+        const filterProperties = await connection.query(
+            `SELECT p.id FROM properties p
+              WHERE p.country LIKE ?
+              AND p.price <= ?
+              AND p.bedrooms >=?
+              AND p.id NOT IN (?)`,
+            [country, maxPrice, minRooms, ids]
+        )
+
+        return filterProperties[0]
+    } finally {
+        if (connection) connection.release()
+    }
+}
+
+export default selectFilterPropertiesModel
