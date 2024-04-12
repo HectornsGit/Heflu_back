c0c3490b931cc98927f87ab53603db41f9ff2de7
HectornsGit <hector.novoa.segade@gmail.com>
2024-04-12 17:47:39 +0200

    refactor: Joins  the  get properties endpoints in the same route

--- controllers/properties/getAllPropertiesController.js
+++ controllers/properties/getAllPropertiesController.js
@@ -1,9 +1,46 @@
 import selectAllPropertiesModel from "../../models/properties/selectAllPropertiesModel.js"
+import selectFilterPropertiesByDatesModel from "../../models/properties/selectFilterPropertiesByDatesModel.js"
+import selectFilterPropertiesModel from "../../models/properties/selectFilterPropertiesModel.js"
+import selectPropertyDetailsModel from "../../models/properties/selectPropertyDetailsModel.js"
 import generateError from "../../scripts/generateError.js"
 
 const getAllPropertiesController = async (req, res, next) => {
     try {
-        const properties = await selectAllPropertiesModel()
+        const params = {
+            country: req.query.country,
+            maxPrice: req.query.maxPrice,
+            minRooms: req.query.minRooms,
+            startDate: req.query.startDate,
+            endDate: req.query.endDate,
+        }
+
+        let properties = []
+
+        if (!Object.keys(req.query).length > 0) {
+            properties = await selectAllPropertiesModel()
+        } else {
+            const filterPropertiesByDate =
+                await selectFilterPropertiesByDatesModel(params)
+            const ids = filterPropertiesByDate.map(
+                (property) => property.property_id
+            )
+
+            const filterProperties = await selectFilterPropertiesModel(
+                params,
+                ids
+            )
+
+            if (filterProperties.length < 1) {
+                throw generateError(404, "No se han encontrado propiedades")
+            }
+
+            const propertyIds = filterProperties.map((property) => property.id)
+
+            const filterPropertiesDetails =
+                await selectPropertyDetailsModel(propertyIds)
+
+            properties = filterPropertiesDetails
+        }
 
         // Si viene vacío, lanzamos un error
         if (properties.lenght < 1) {
