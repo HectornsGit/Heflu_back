e786ee39c4adaf817d4a7359f3931b6d11c9e0fa
HectornsGit <hector.novoa.segade@gmail.com>
2024-04-12 17:47:39 +0200

    refactor: Joins  the  get properties endpoints in the same route

--- api/propertiesRoutes.js
+++ api/propertiesRoutes.js
@@ -3,14 +3,12 @@ import express from "express"
 import newPropertyController from "../controllers/properties/newPropertyController.js"
 import getAllPropertiesController from "../controllers/properties/getAllPropertiesController.js"
 import getPropertyController from "../controllers/properties/getPropertyController.js"
-import getFilterPropertiesController from "../controllers/properties/getFilterPropertiesController.js"
 
 // Middlewares
 import isUser from "../middlewares/isUser.js"
 
 const router = express.Router()
 
-router.get("/filter/?", getFilterPropertiesController)
 router.post("/", isUser, newPropertyController)
 router.get("/", getAllPropertiesController)
 router.get("/:id", getPropertyController)
