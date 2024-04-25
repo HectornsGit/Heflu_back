2409d555e4119543886470adfda0d540917c3921
HectornsGit <hector.novoa.segade@gmail.com>
2024-04-25 11:36:21 +0200

    Feat: now sends an actual link of the frontend.

--- scripts/sendBookingNotificationEmail.js
+++ scripts/sendBookingNotificationEmail.js
@@ -1,5 +1,5 @@
 import { Resend } from "resend"
-import { PORT, RESEND_API_KEY } from "../config/env.js"
+import { FRONTEND_PORT, RESEND_API_KEY } from "../config/env.js"
 
 const sendBookingNotificationEmail = (email, property) => {
     const resend = new Resend(RESEND_API_KEY)
@@ -10,7 +10,7 @@ const sendBookingNotificationEmail = (email, property) => {
         html: `<h1>Buenas, has recibido una nueva reserva</h1>
         <h2><em><strong>${property.name} - ${property.location}</strong></em></h2>
         <p><strong>Comprueba en el siguiente enlace tus reservas pendientes</strong></p>
-        <h1><span style="text-decoration: underline;"><a href="http://localhost:${PORT}/bookings/">RESERVAS</a></span></h1>
+        <h1><span style="text-decoration: underline;"><a href="http://localhost:${FRONTEND_PORT}/profile/requests">RESERVAS</a></span></h1>
         <h2>HEFLÚ</h2>`,
     })
 }
--- scripts/sendRegistrationCodeEmail.js
+++ scripts/sendRegistrationCodeEmail.js
@@ -1,5 +1,5 @@
 import { Resend } from "resend"
-import { PORT, RESEND_API_KEY } from "../config/env.js"
+import { FRONTEND_PORT, RESEND_API_KEY } from "../config/env.js"
 
 const sendRegistrationCodeEmail = (email, registration_code) => {
     const resend = new Resend(RESEND_API_KEY)
@@ -10,7 +10,7 @@ const sendRegistrationCodeEmail = (email, registration_code) => {
         html: `<h1>Buenas, te has registrado correctamente</h1>
         <h3><em>Solo queda un pequeño paso,</em></h3>
         <p><strong>Activa tu cuenta en el siguiente enlace</strong></p>
-        <h1><span style="text-decoration: underline;"><a href="http://localhost:${PORT}/users/validate/${registration_code}">ACTIVAR</a></span></h1>
+        <h1><span style="text-decoration: underline;"><a href="http://localhost:${FRONTEND_PORT}/users/validate/${registration_code}">ACTIVAR</a></span></h1>
         <h2>¡Date prisa!</h2>
         <p><em><strong>¡Tus vacaciones de ensueño te esperan!</strong></em></p>
         <h2>HEFLÚ</h2>`,
