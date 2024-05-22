dd04b9180e6791af97eae2956f67ecd57a460779
HectornsGit <hector.novoa.segade@gmail.com>
2024-05-22 18:30:52 +0200

    feat: adds an endpoint to get bookings by tenant id

---
+++ controllers/bookings/getUserBookingsController.js
@@ -0,0 +1,30 @@
+import selectBookingsByTenantIdModel from "../../models/bookings/selectBookingsByTenantId.js"
+const getUserBookingsController = async (req, res, next) => {
+    try {
+        const { id } = req.user
+        const bookings = await selectBookingsByTenantIdModel(id)
+
+        if (bookings.length < 1) {
+            return res.status(204).send({
+                status: "ok",
+                data: [],
+                message: "¡No has participado en ninguna estancia todavía!",
+            })
+        }
+
+        let formattedBookings = []
+
+        for (let booking of bookings) {
+            booking.images = booking.images.split(",")
+            formattedBookings.push(booking)
+        }
+        res.status(200).send({
+            status: "ok",
+            data: formattedBookings,
+        })
+    } catch (err) {
+        next(err)
+    }
+}
+
+export default getUserBookingsController
