75780112d5f74f5ab100f8498f29ea95e36a9f40
HectornsGit <hector.novoa.segade@gmail.com>
2024-05-22 13:06:13 +0200

    fix: adds data object to the no properties response

--- controllers/users/getUserController.js
+++ controllers/users/getUserController.js
@@ -1,6 +1,7 @@
 import generateError from "../../scripts/generateError.js"
 import selectUserByIdModel from "../../models/users/selectUserByIdModel.js"
 import selectPropertiesByOwnerIdModel from "../../models/properties/selectPropertiesByOwnerIdModel.js"
+import selectPropertyDetailsModel from "../../models/properties/selectPropertyDetailsModel.js"
 
 const getUserController = async (req, res, next) => {
     const { id } = req.params
@@ -21,10 +22,27 @@ const getUserController = async (req, res, next) => {
             media_rating: user.media_rating,
         }
         const userProperties = await selectPropertiesByOwnerIdModel(id)
+        if (userProperties.length > 0) {
+            const userPropertyIds = userProperties.map(
+                (property) => property.id
+            )
 
-        res.status(200).send({
-            data: { user: userData, properties: userProperties },
-        })
+            const userPropertiesDetails =
+                await selectPropertyDetailsModel(userPropertyIds)
+
+            // Cambiar formato de las imágenes a un array
+            let formattedProperties = []
+
+            for (let property of userPropertiesDetails) {
+                property.property_images = property.property_images.split(",")
+                formattedProperties.push(property)
+            }
+
+            res.status(200).send({
+                data: { user: userData, properties: formattedProperties },
+            })
+        }
+        res.status(200).send({ data: { user: userData, properties: [] } })
     } catch (error) {
         next(error)
     }
